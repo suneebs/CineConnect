e0d8018771e8768f0c47d30e5adb3ab8ea0727ae
suneeb <ssuneebvishnu@gmail.com>
2025-03-24 12:05:01 +0530

    Refactor SuggestedUsers and TalentHeader components for improved functionality and styling; clean up AuthPage layout

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -25,12 +25,7 @@ const AuthPage = () => {
 			</video>
 			<Container maxW={"container.md"} padding={0}>
 				<Flex justifyContent={"center"} alignItems={"center"} gap={10}>
-					{/* Left hand-side */}
-					{/* <Box display={{ base: "none", md: "block" }}>
-						<Image src='/auth.webp' h={600} alt='Cinema img' />
-					</Box> */}
 
-					{/* Right hand-side */}
 					<VStack spacing={1} align={"stretch"}>
 						<AuthForm />
 						{/* <Box textAlign={"center"}>Get the app.</Box> */}
